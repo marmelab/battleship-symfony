812641349304cd19835d2fe57e96b2560fc92089
Benjamin Payoux <benjamin.payoux@protonmail.ch>
2020-09-28 17:59:44 +0200

    Add service to get a game by hash

--- app/src/Controller/Api/GameController.php
+++ app/src/Controller/Api/GameController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Game;
 use App\GameManipulator;
 use App\Repository\GameRepository;
+use App\Repository\ShipRepository;
+use App\Twig\AppExtension;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class GameController extends AbstractController
 {
@@ -24,6 +28,35 @@ class GameController extends AbstractController
             'status' => 200,
             'success' => "Game created successfully",
             'game_hash' => $game->getHash()
+        ]);
+    }
+
+    /**
+     * Get a game
+     * 
+     * @ParamConverter("game", options={"mapping": {"hash": "hash"}})
+     * @param GameRepository $gameRepository
+     * @return JsonResponse
+     * @Route("/api/games/{hash}", name="games_show", methods={"GET"})
+     */
+    public function show(Game $game, ShipRepository $shipRepository, AppExtension $appExtension)
+    {
+        $ships = $shipRepository->getCurrentPlayerShips($game);
+
+        $array =[];
+
+        foreach ($ships as $ship) {
+            $array[] = [
+                'coordinates' => $ship->getCoordinates(),
+                'grid_attributes' => $appExtension->getGridAttributes($ship)
+            ];  
+        }
+
+        return $this->json([
+            'status' => 200,
+            'success' => "Game fetched successfully",
+            'game_hash' => $game->getHash(),
+            'ships' => $array
         ]);
     }
 }
